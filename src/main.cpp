a934c5e77acdf4794216d7420a507b84102a42ac
Nick Sharp <nsharp@cs.cmu.edu>
2019-07-21 22:06:39 -0400

    partial work

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "geometrycentral/surface/halfedge_mesh.h"
 #include "geometrycentral/surface/heat_method_distance.h"
 #include "geometrycentral/surface/meshio.h"
+#include "geometrycentral/surface/vector_heat_method.h"
 #include "geometrycentral/surface/vertex_position_geometry.h"
 
 #include "polyscope/polyscope.h"
@@ -8,6 +9,8 @@
 
 #include "args/args.hxx"
 #include "imgui.h"
+
+#include <sstream>
 
 using namespace geometrycentral;
 using namespace geometrycentral::surface;
@@ -17,51 +20,190 @@ std::unique_ptr<HalfedgeMesh> mesh;
 std::unique_ptr<VertexPositionGeometry> geometry;
 
 // Polyscope visualization handle, to quickly add data to the surface
-polyscope::SurfaceMesh *psMesh;
+polyscope::SurfaceMesh* psMesh;
 
 // Some algorithm parameters
 float tCoef = 1.0;
-std::unique_ptr<HeatMethodDistanceSolver> solver;
+std::unique_ptr<VectorHeatMethodSolver> solver;
 int vertexInd = 0;
 
-// Example computation function -- this one computes and registers a scalar
-// quantity
-void doWork() {
+// Manage a list of sources
+struct SourceVert {
+  Vertex vertex;
+  float scalarVal = 1.;
+  float vectorMag = 1.;
+  float vectorAngleRad = 0.;
+};
+std::vector<SourceVert> sourcePoints;
+
+bool vizFirstRun = true;
+void updateSourceSetViz() {
+
+  // Scalar balls around sources
+  std::vector<std::pair<size_t, double>> sourcePairs;
+  for (SourceVert& s : sourcePoints) {
+    size_t ind = geometry->vertexIndices[s.vertex];
+    sourcePairs.emplace_back(ind, s.scalarVal);
+  }
+  auto scalarQ = polyscope::getSurfaceMesh()->addVertexIsolatedScalarQuantity("source scalars", sourcePairs);
+  scalarQ->pointRadius *= 2.;
+  scalarQ->cMap = polyscope::gl::ColorMapID::REDS;
+  if (vizFirstRun) {
+    scalarQ->setEnabled(true);
+  }
+
+  // Vectors at sources
+  VertexData<Vector2> sourceVectors(*mesh, Vector2::zero());
+  for (SourceVert& s : sourcePoints) {
+    sourceVectors[s.vertex] = Vector2::fromAngle(s.vectorAngleRad) * s.vectorMag;
+  }
+  auto vectorQ = polyscope::getSurfaceMesh()->addVertexIntrinsicVectorQuantity("source vectors", sourceVectors);
+  vectorQ->lengthMult *= 2.;
+  vectorQ->radiusMult *= 4.;
+  vectorQ->vectorColor = glm::vec3{227 / 255., 52 / 255., 28 / 255.};
+  if (vizFirstRun) {
+    vectorQ->setEnabled(true);
+  }
+
+  vizFirstRun = false;
+}
+
+void addVertexSource(size_t ind) {
+  Vertex v = mesh->vertex(ind);
+
+  // Make sure not already used
+  for (SourceVert& s : sourcePoints) {
+    if (s.vertex == v) {
+      std::stringstream ss;
+      ss << "Vertex " << v;
+      std::string vStr = ss.str();
+      polyscope::warning("Vertex " + vStr + " is already a source");
+      return;
+    }
+  }
+
+  SourceVert newV;
+  newV.vertex = v;
+  sourcePoints.push_back(newV);
+  updateSourceSetViz();
+}
+
+void scalarExtension() {
 
   if (solver == nullptr) {
-    solver.reset(new HeatMethodDistanceSolver(*geometry, tCoef));
+    solver.reset(new VectorHeatMethodSolver(*geometry, tCoef));
   }
 
-  /*
-  std::vector<int> inds{1631};
-  std::vector<SurfacePoint> points;
-  for (int ind : inds) {
-    Face f = mesh->face(ind);
-    points.emplace_back(f, Vector3::constant(1. / 3.));
+  if (sourcePoints.size() == 0) {
+    polyscope::warning("no source points set");
+    return;
   }
-  VertexData<double> distance = solver->computeDistance(points);
-  */
 
-  VertexData<double> distance =
-      solver->computeDistance(mesh->vertex(vertexInd));
+  std::vector<std::tuple<SurfacePoint, double>> points;
+  for (SourceVert& s : sourcePoints) {
+    points.emplace_back(s.vertex, s.scalarVal);
+  }
 
-  psMesh->addVertexDistanceQuantity("distance", distance);
+  VertexData<double> scalarExtension = solver->extendScalar(points);
+
+  psMesh->addVertexScalarQuantity("scalar extension", scalarExtension);
+}
+
+void vectorTransport() {
+
+  if (solver == nullptr) {
+    solver.reset(new VectorHeatMethodSolver(*geometry, tCoef));
+  }
+
+  if (sourcePoints.size() == 0) {
+    polyscope::warning("no source points set");
+    return;
+  }
+
+  std::vector<std::tuple<SurfacePoint, Vector2>> points;
+  for (SourceVert& s : sourcePoints) {
+    points.emplace_back(s.vertex, Vector2::fromAngle(s.vectorAngleRad) * s.vectorMag);
+  }
+  VertexData<Vector2> vectorExtension = solver->transportTangentVectors(points);
+
+  psMesh->addVertexIntrinsicVectorQuantity("vector extension", vectorExtension);
+}
+
+void buildPointsMenu() {
+
+  bool anyChanged = false;
+
+  ImGui::PushItemWidth(200);
+
+  int id = 0;
+  int eraseInd = -1;
+  for (SourceVert& s : sourcePoints) {
+    std::stringstream ss;
+    ss << "Vertex " << s.vertex;
+    std::string vStr = ss.str();
+    ImGui::PushID(vStr.c_str());
+
+    ImGui::TextUnformatted(vStr.c_str());
+
+    ImGui::SameLine();
+    if (ImGui::Button("delete")) {
+      eraseInd = id;
+      anyChanged = true;
+    }
+    ImGui::Indent();
+
+    if (ImGui::InputFloat("scalar value", &s.scalarVal)) anyChanged = true;
+    if (ImGui::InputFloat("vector mag", &s.vectorMag)) anyChanged = true;
+    if (ImGui::SliderAngle("vector angle", &s.vectorAngleRad)) anyChanged = true;
+
+    ImGui::Unindent();
+    ImGui::PopID();
+  }
+  ImGui::PopItemWidth();
+
+  // actually do erase, if requested
+  if (eraseInd != -1) {
+    sourcePoints.erase(sourcePoints.begin() + eraseInd);
+  }
+
+  if (ImGui::Button("add point")) {
+    long long int pickVert = polyscope::getSurfaceMesh()->selectVertex();
+    if (pickVert >= 0) {
+      addVertexSource(pickVert);
+      anyChanged = true;
+    }
+  }
+
+  if (anyChanged) {
+    updateSourceSetViz();
+  }
 }
 
 void myCallback() {
 
-  if (ImGui::Button("make it so")) {
-    doWork();
-  }
-
+  ImGui::TextUnformatted("Algorithm options:");
+  ImGui::PushItemWidth(300);
   if (ImGui::InputFloat("tCoef", &tCoef)) {
     solver.reset();
   }
+  ImGui::PopItemWidth();
 
-  ImGui::InputInt("vertex index", &vertexInd);
+  // Build the list of source points
+  if (ImGui::TreeNode("source points")) {
+    buildPointsMenu();
+    ImGui::TreePop();
+  }
+
+  if (ImGui::Button("run scalar extension")) {
+    scalarExtension();
+  }
+
+  if (ImGui::Button("run vector transport")) {
+    vectorTransport();
+  }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 
   // Configure the argument parser
   args::ArgumentParser parser("A demo of the Vector Heat Method");
@@ -95,10 +237,31 @@ int main(int argc, char **argv) {
   std::tie(mesh, geometry) = loadMesh(args::get(inputFilename));
 
   // Register the mesh with polyscope
-  psMesh = polyscope::registerSurfaceMesh(
-      polyscope::guessNiceNameFromPath(args::get(inputFilename)),
-      geometry->inputVertexPositions, mesh->getFaceVertexList(),
-      polyscopePermutations(*mesh));
+  psMesh = polyscope::registerSurfaceMesh(polyscope::guessNiceNameFromPath(args::get(inputFilename)),
+                                          geometry->inputVertexPositions, mesh->getFaceVertexList(),
+                                          polyscopePermutations(*mesh));
+
+
+  // Set vertex tangent spaces
+  geometry->requireVertexTangentBasis();
+  VertexData<Vector3> vBasisX(*mesh);
+  for(Vertex v : mesh->vertices()) {
+    vBasisX[v] = geometry->vertexTangentBasis[v][0];
+  }
+  polyscope::getSurfaceMesh()->setVertexTangentBasisX(vBasisX);
+  
+  // Set face tangent spaces
+  geometry->requireFaceTangentBasis();
+  FaceData<Vector3> fBasisX(*mesh);
+  for(Face f : mesh->faces()) {
+    fBasisX[f] = geometry->faceTangentBasis[f][0];
+  }
+  polyscope::getSurfaceMesh()->setFaceTangentBasisX(fBasisX);
+
+  // To start, pick two vertices as sources
+  geometry->requireVertexIndices();
+  addVertexSource(0);
+  addVertexSource(mesh->nVertices() / 2);
 
   // Give control to the polyscope gui
   polyscope::show();
